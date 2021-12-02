c11af889dc7990fd98c37818a1abb8071ec6c5a1
Zwitschi <zwitschi82@gmail.com>
2021-12-02 18:08:56 +0100

    testing javascript functionalities

--- js/script.js
+++ js/script.js
@@ -1,1 +1,28 @@
-/* Codebase for zwitschi.github.io */
+/**
+ * Codebase for zwitschi.github.io
+ * 
+ * @author: Georg Sinn <zwitschi82 at gmail dot com>
+ * 
+ * @license: GNU GPLv3
+ * 
+ * @version: 0.1.0
+ * 
+ */
+
+/**
+ * @name: script
+ * 
+ * @description: main script
+ */
+function init() {
+    // init  
+    get_site_copy_year()  
+}
+
+function get_site_copy_year() {
+    var container_site_copy_year = $('#site-copy-year')
+    alert("Old Year: "+container_site_copy_year)
+    container_site_copy_year.text(new Date().getFullYear())
+}
+
+body.onload(init())
